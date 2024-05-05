93fc903d15525b78f4f29a46ac328b95be88455c
vasudev290 <vasukesavulu@gmail.com>
2024-05-05 15:57:42 +0530

    Object

---
+++ jsone/Operators/Controlflow/Iterativeflow/forin/one.js
@@ -0,0 +1,14 @@
+//sample Forin object function....
+
+let emp = {id:131, 
+           first_name: "Vasu", 
+           Last_name:"Dev", 
+           Email: "Vasukesavulu@gmail.com", 
+           Loc: ['Chennai', 'Bangalore'], 
+           Sal: 55000, 
+           Designation:"Web Devloper"
+        };
+
+        for(key in emp){
+            console.log(emp[key]);
+        }
---
+++ jstwo/Object/one.js
@@ -0,0 +1,12 @@
+//sample object function....
+
+let emp = {id:101, Name:"Vasu Devu", Sal:55000}
+console.log(emp);
+
+emp.id
+emp.Name
+emp.Sal
+emp.address
+console.log(emp);
+emp.loc ="Bangalore"
+console.log(emp);
