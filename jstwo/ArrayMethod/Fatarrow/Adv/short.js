c24dd76722e0a425d6c0fa7826d523cb9a34470b
vasudev290 <vasukesavulu@gmail.com>
2024-05-05 21:34:11 +0530

    Fatarrow

---
+++ jstwo/ArrayMethod/Fatarrow/Adv/short.js
@@ -0,0 +1,11 @@
+//shirt cut of fatarroe function...
+
+ /* let add = (a,b)=>{
+    return a+b
+ }
+ let result = add(10,20)
+ console.log(result); */
+
+ let add = (a,b)=> a+b;
+ let Result = add (100, 200)
+ console.log(Result);
