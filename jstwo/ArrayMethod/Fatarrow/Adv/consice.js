b2f040657f56d391c215f4cd6efd78719f2cbdd7
vasudev290 <vasukesavulu@gmail.com>
2024-05-06 17:20:05 +0530

    Data table

--- jstwo/ArrayMethod/Fatarrow/Adv/consice.js
+++ jstwo/ArrayMethod/Fatarrow/Adv/consice.js
@@ -1,9 +1,9 @@
 // consice the normal function and fatarrow function...
 
-function message(a){
+/* function message(a){
     return "hello" + a
 }
-console.log(message("Good morning"));
+console.log(message("Good morning")); */
 
 
 let message = msg => "hello" + msg
---
+++ jstwo/ArrayMethod/Stringliteral/one.js
@@ -0,0 +1,10 @@
+
+//sample of string literal statements...
+
+var name = "Vasu Dev"
+var sal = 55000
+
+console.log("Employee name:" + name + " and Employee salary:" + sal);
+console.log(`Employee name: ${name} & Employee salary: ${sal}`);
+console.log(`${name}`);
+console.log(`${sal}`);
