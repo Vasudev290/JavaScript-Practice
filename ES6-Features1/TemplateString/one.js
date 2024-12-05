a204ce2cfa156aa4fe336cdc6936ca0f49bbab28
Vasudev290 <vasukesavulu@gmail.com>
2024-12-05 22:59:55 +0530

    es6

---
+++ ES6-Features1/TemplateString/one.js
@@ -0,0 +1,18 @@
+//Example 1
+var Example = "Cricket"
+
+document.write("I ❤️ Playing : " + Example + "<br>");
+
+document.write(`I ❤️ Playing :${Example}<br/>`);
+
+console.log(`I ❤️ Playing :${Example}`)
+
+
+//Example - 2
+var name = "Dhoni";
+
+var profession = "Cricket"
+
+var gamePlan = "SmartPlay"
+
+document.write(`${name} Very Good ${profession} Player and Good ${gamePlan}`)
