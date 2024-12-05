81f368040d8580551898635d03a1cbe697dd42e6
Vasudev290 <vasukesavulu@gmail.com>
2024-12-05 23:21:58 +0530

    Arrow Function

---
+++ ES6-Features1/ArrowFunction -2/one.js
@@ -0,0 +1,26 @@
+//Function Method
+function apple () {
+    document.write("I Like Apples<br>")
+}
+apple()
+
+//Expersion Method
+
+const mongo = function mongo(){
+    document.write("Mongoos are too sweety..!<br>")
+}
+mongo()
+
+
+//Arrow Function
+const grapes = () => {
+    document.write("I Like Grapes Juice<br>" )
+}
+grapes()
+
+
+//Arrow Function (Parameters)
+const addition = (a,b) => {
+    document.write(a+b)
+}
+addition(100, 200)
