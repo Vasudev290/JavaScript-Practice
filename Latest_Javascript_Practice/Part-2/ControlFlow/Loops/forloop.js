b4d7b51a42b68df26767793615bef965a0e74e58
Vasudev290 <vasukesavulu@gmail.com>
2025-02-14 12:50:53 +0530

    loops

---
+++ Latest_Javascript_Practice/Part-2/ControlFlow/Loops/forloop.js
@@ -0,0 +1,42 @@
+//for loop -> A loops that runs for a specific number of iterations.
+//determind by the -> initialistion, conditions, incr/decr
+
+//print 1 to 10
+for(let i=1; i<=10; i++){
+    console.log( i)
+}
+
+//print 10 to 6
+for(let i=10; i>=6; i--){
+    console.log(i)
+}
+
+//Write a program to print 1 to 100 numbers using a for loop
+for(let i=1; i<=100; i++){
+    console.log(i)
+}
+
+//Write a program to print 100 to 1 numbers using a for loop
+for(i=100; i>=1; i--){
+    console.log(i)
+}
+
+//Write a program to print 50 to 150 numbers using a for loop
+for(let i=50; i<=150; i++){
+    console.log(i)
+}
+
+//Write a program to print 15 to 10 numbers using a for loop
+for(let i=15; i>=10; i--){
+    console.log(i)
+}
+
+//Write a program to print 1 to 10 even numbers using a for loop
+for(let i=2; i<=10; i+=2){
+    console.log(i )
+}
+
+//Write a program to print 1 to 10 odd numbers using a for loop
+for(i=1; i<=10; i+=2){
+    console.log(i)
+}
